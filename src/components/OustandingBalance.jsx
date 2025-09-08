a93d4c0d706b026f256dcb003e8d5a58ca80bef3
GustavoLuizaga <luizagamerinogustavo1@gmail.com>
2025-09-07 20:42:31 -0400

    change numbers cols ouCards

--- src/components/OustandingBalance.jsx
+++ src/components/OustandingBalance.jsx
@@ -3,7 +3,7 @@ import { OutstandingBalanceCard } from "./OustandingBalanceCard";
 export function OutstandingBalance({balanceCards}) {
     return (
 
-        <div className="grid grid-cols-2 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-3 sm:gap-6 lg:gap-8">
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-3 sm:gap-6 lg:gap-8">
             {balanceCards.map((cardData) => (
                 <OutstandingBalanceCard
                     key={cardData.id}
